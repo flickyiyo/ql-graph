55002e5c3d5234e9f75ed23f03289e82314c6a93
Edgar <edgar21_9@hotmail.com>
2022-01-19 00:07:26 -0600

    Added cases for match and create

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,54 @@
+pub struct Node {}
+
+#[derive(Debug)]
+pub struct Query<'a> {
+    pub query_string: &'a String,
+}
+
+impl<'a> Query<'a> {
+    pub fn new(query_string: &'a String) -> Self {
+        Self { query_string }
+    }
+
+    pub fn from_query(&mut self) {
+        
+        let splt = self.query_string.split(" ");
+
+        let vec: Vec<&str> = splt.collect();
+
+        for item in vec {
+            match item.to_lowercase().as_str() {
+                "match" => {
+                    
+                },
+                "create" => {
+
+                },
+                x => {
+                    if x != " " {
+
+                    } 
+                    if x == ";" {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn detects_match() {
+        let query = "MATCH (:Person {name: \"Dan\"}) - [:LOVES] -> (whom) RETURN whom;";
+
+
     }
 }
